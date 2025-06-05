ed42342adb56a1c4b21aade564d1538379a9294f
jushinpon <jushinpon@gmail.com>
2025-06-05 17:26:40 +0800

    modify data for scale

--- data_collect4Scale.pl
+++ data_collect4Scale.pl
@@ -24,16 +24,16 @@ my %dir4scale;
 ## IMPORTANT!! You need to make sure no data file name is identical within the scaling folders (scale_2dab, scale_3d...)
 #$dir4scale{scale_1da} = ();
 
-#$dir4scale{scale_2dab} = [
-#    #"/home/jsp/SnPbTe_alloys/make_surface_20240919/add_vacuum/" #better to do vc-md or ve-relax first
-#];
+$dir4scale{scale_2dab} = [
+   "/home/shihtsao/MS_Relax2Md_QE_from_MatCld/QEall_set_for_Scale/" #better to do vc-md or ve-relax first
+];
 
 #"/home/jsp/SnPbTe_alloys/QE_from_MatCld/cif2data/  only use materials project strutures
 #under QEall_set, you need to provide the data_files folder  
-$dir4scale{scale_3d} = [
+# $dir4scale{scale_3d} = [
     #"/home/jsp/SnPbTe_alloys/QE_from_MatCld/cif2data/",
-    "/home/jsp/SnPbTe_alloys/make_B2_related_data/QEall_set/"
-];
+#     "/home/jsp/SnPbTe_alloys/make_B2_related_data/QEall_set/"
+# ];
 
 `rm -rf data4scale`;
 `mkdir -p data4scale`;
@@ -52,105 +52,42 @@ for my $type (sort keys %dir4scale){#scaling type
     my @temp_dirs = @{$dir4scale{$type}};
     
     for my $p (@temp_dirs){# all source dirs
-        #my $dir = `dirname $p`;
-        #$dir = s/^\s+|\s+$//g;
-        #print "\$p: $p, \$dir: $dir\n";
-
-        if($p =~ m|.+/QEall_set/.*|){
-            my @datafiles = `find -L $p -type f -name "*.data"`;#find all data files
-            map { s/^\s+|\s+$//g; } @datafiles;
-            die "No data files to collect under $p!\n" unless(@datafiles);
-            #get the last MD data file at the lowest temperature for scaling
-            my %Str_minT;
-            my %Str_path;
-            for my $data (reverse sort @datafiles){#get the data file with the largest index
-                $data =~ m|.+/([^/]+)-T(\d+)-P\d+/data_files/.+\.data|;
-                die "No formula or temperature is captured for $data\n" unless($1 or $2);
-                #print "**$data\n";
-                #print "$QEin\n";
-                #print "$data\n";
-                if(exists $Str_minT{$1}){
-                    if($Str_minT{$1} > $2){$Str_minT{$1} = $2;$Str_path{$1} = $data;}
-                }
-                else{
-                    $Str_minT{$1} = $2;
-                    $Str_path{$1} = $data;
+        my @datafiles = `find -L $p -type d -name "data_files"`;#Find all data_files directories
+        map { s/^\s+|\s+$//g; } @datafiles;
+        die "No data_files folder under $p!\n" unless(@datafiles);
+        
+        my %lowest_temp;#get the lowest temperature for each prefix
+        
+        for my $path (@datafiles) {
+            if ($path =~ m|(.*)/([^/]+)-T(\d+)-P\d+/data_files|) {
+                my ($dir, $prefix, $temp) = ($1, $2, $3);
+                if (!exists $lowest_temp{$prefix} or $temp < $lowest_temp{$prefix}{temp}) {
+                    $lowest_temp{$prefix} = { dir => $dir, folder => "$prefix-T$temp-P0"};
                 }
             }
-        #get the lowest temperature
-
-            for my $key (keys %Str_minT){
-               # print "$key, $Str_minT{$key}\n";
-                #print "$key, $Str_path{$key}\n";
-                my $temp = "$key"."_T$Str_minT{$key}";
-                `mkdir -p ./data4scale/$type/$temp`;
-                #my $qein = /data_files/099.data;
-                print "\$Str_path{$key}:$Str_path{$key}\n";
-                #get the corresponding QE input
-                $Str_path{$key} =~ m|(.+/[^/]+-T\d+-P\d+)/data_files/.+\.data|;                
-                die "No QE input for $Str_path{$key}\n" unless($1);
-                #my $QEin_dir = $1;
-                my $QEin = `ls $1/*.in`;
-                $QEin =~ s/^\s+|\s+$//g;        
-
-                `cp $QEin ./data4scale/$type/$temp/ori.in`;
-                `cp $Str_path{$key} ./data4scale/$type/$temp/$temp.data`;
-                `cd ./data4scale/$type/$temp/;perl $currentPath/Mod_data_eleType.pl $temp.data`;
-                `mv ./data4scale/$type/$temp/output.data ./data4scale/$type/$temp/$temp.lmp`;
-                system("atomsk ./data4scale/$type/$temp/$temp.lmp ./data4scale/$type/$temp/ori.cif");
-            }
-
         }
-        else{#No /QEall_set/ (for scf only)
-            my @datafiles = `find -L $p -type f -name "*.data"`;#find all data files
-            map { s/^\s+|\s+$//g; } @datafiles;
-            die "No data files to collect under $p!\n" unless(@datafiles);
-            #get the last MD data file at the lowest temperature for scaling
-            for my $data (@datafiles){
-                #print "$data\n";
-                my $temp = `basename $data\n`;
-                $temp =~ s/^\s+|\s+$//g;
-                $temp =~ s/\.data$//;
-                
-                $data =~ m|/(QE_from_MatCld)/|;
-                if($1){
-                    $data =~ m|(.*?)/[^/]+/[^/]+$|;
-                    print "\$data: $data\n";
-                    my $temp_dir = $1;
-                    die "No Qe input for $data\n" unless($temp_dir);
-                    my $QEin = `ls $temp_dir/QE_trimmed/$temp.in`;
-                    $QEin =~ s/^\s+|\s+$//g;
-                    die "No QE input of $data\n" unless($QEin);    
-                    `mkdir -p ./data4scale/$type/$temp`;
-                    `cp $QEin ./data4scale/$type/$temp/ori.in`;
-                }
-                else{
-                    $data =~ m|(.*?)/[^/]+/[^/]+$|;
-                    #print "\$data: $data\n";
-                    my $temp_dir = $1;
-                    die "No Qe input for $data\n" unless($temp_dir);
-                    my $QEin = `ls $temp_dir/QE_trimmed/$temp/$temp.in`;
-                    $QEin =~ s/^\s+|\s+$//g;
-                    die "No QE input of $data\n" unless($QEin);    
-                    `mkdir -p ./data4scale/$type/$temp`;
-                    `cp $QEin ./data4scale/$type/$temp/ori.in`;
-                }
-                `cp $data ./data4scale/$type/$temp/$temp.data`;
-                #`cp $data ./data4scale/$type/$temp/$temp.lmp`;
-                `cd ./data4scale/$type/$temp/;perl $currentPath/Mod_data_eleType.pl $temp.data`;
-                `mv ./data4scale/$type/$temp/output.data ./data4scale/$type/$temp/$temp.lmp`;
-                
-                #need to modify lmp with proper atom type number
-                
-                system("atomsk ./data4scale/$type/$temp/$temp.lmp ./data4scale/$type/$temp/ori.cif");
-
-                #$data =~ m|.+/([^/]+)-T(\d+)-P\d+/data_files/.+\.data|;
-            }
-
+        #begin to collect the lowest temperature data files
+        for my $prefix (keys %lowest_temp) {
+            #print "Lowest temperature folder for $prefix:\n";
+            my $dir = $lowest_temp{$prefix}{dir};
+            my $folder = $lowest_temp{$prefix}{folder};
+            my $QEin = "$dir/$folder/$folder.in";
+            `mkdir -p ./data4scale/$type/$folder`;
+            #copy the QE input file for kpoints
+            die "No QE input file for $QEin\n" unless(-e $QEin);
+            `cp  $QEin ./data4scale/$type/$folder/ori.in`;
+            
+            #copy the last data file of vc-md or md for the base structure
+            my @data_file = glob("$dir/$folder/data_files/*.data");#get all data files under the lowest temperature folder
+            map { s/^\s+|\s+$//g; } @data_file;
+            @data_file = sort @data_file;
+            die "No data files under $dir/$folder/data_files!\n" unless(@data_file);
+            my $data = pop @data_file;#get the last data file
+            `cp $data ./data4scale/$type/$folder/$folder.data`;
+            `cp $data ./data4scale/$type/$folder/$folder.lmp`;#for atomsk
+            system("atomsk ./data4scale/$type/$folder/$folder.lmp ./data4scale/$type/$folder/ori.cif");            
         }
-
-    }
-
-}
+    }#all data_files
+}#all scaling types
 
 
